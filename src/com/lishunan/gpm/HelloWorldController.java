1661bb7003b50d7b0b090339b5c248fb73fca8c9
Li Shunan <lishunan246@gmail.com>
2015-01-30 18:38:44 +0800

    servlet test

---
+++ src/com/lishunan/gpm/HelloWorldController.java
@@ -0,0 +1,21 @@
+package com.lishunan.gpm;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by lishunan on 15-1-30.
+ */
+public class HelloWorldController extends HttpServlet {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        String c = request.getParameter("color");
+        out.print(c);
+    }
+
+}
